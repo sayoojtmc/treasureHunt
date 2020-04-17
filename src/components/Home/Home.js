72e0ecf3bd4fb6d9fae7df0ff5b72a49f90521a5
sayoojtmc <sayoojtmc@gmail.com>
2020-04-17 09:06:55 +0530

    test image

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,17 +2,12 @@ import React, { Component } from "react";
 import { Navbar, Jumbotron } from "react-bootstrap";
 import withAuthorization from "../Session/withAuthorization";
 import Firebase from "../Firebase/Firebase";
-import withFireBase from "../Firebase/context";
+import { Accordion, Card } from "react-bootstrap";
 class Home extends Component {
   constructor(props) {
     super(props);
   }
-  componentDidMount() {
-    const db = this.props.firebase.firestore;
-    var user = this.props.auth.CurrentUser;
-    db.collection("user");
-    console.log(user);
-  }
+  componentDidMount() {}
   render() {
     return (
       <div>
@@ -22,11 +17,30 @@ class Home extends Component {
             alignItems: "center",
           }}
         >
-          <Jumbotron className="my-5 mx-5">
-            <div>
-              <h1>Insert Image here</h1>
+          <div className="row">
+            <div className="col-md-6 offset-sm-3">
+              <Accordion defaultActiveKey="0">
+                <Card>
+                  <Accordion.Toggle
+                    as={Card.Header}
+                    variant="link"
+                    eventKey="0"
+                  >
+                    How to play
+                  </Accordion.Toggle>
+                  <Accordion.Collapse eventKey="0">
+                    <Card.Body>Rules</Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+                <div></div>
+              </Accordion>
+              <Jumbotron className="my-5 mx-5">
+                <div>
+                  <img src="testimage.jpeg" className="img img-fluid" />
+                </div>
+              </Jumbotron>
             </div>
-          </Jumbotron>
+          </div>
         </div>
       </div>
     );
@@ -35,4 +49,4 @@ class Home extends Component {
 
 const condition = (authUser) => !!authUser;
 
-export default withAuthorization(condition)(withFireBase(Home));
+export default withAuthorization(condition)(Home);
