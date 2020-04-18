bcfd99a7b34c4108d24e77035aa68a8cce7fd860
sayoojtmc <sayoojtmc@gmail.com>
2020-04-18 12:13:48 +0530

    tasks and file upload

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,8 +11,26 @@ class Home extends Component {
       level: 1,
       guesses: 4,
       url: "",
+      file: null,
     };
   }
+  handleChange = (e) => {
+    e.preventDefault();
+    this.setState({ file: e.target.files[0] });
+  };
+  handleUpload = () => {
+    var storage = this.props.firebase.storage;
+
+    var filename = this.state.file.name;
+    var name = filename.split(".")[0];
+    var extension = name.split(".")[1];
+    try {
+      var pathReference = storage.ref(`tasks/${name}.${extension}`);
+      pathReference.put(this.state.file).then((snapshot) => {
+        pathReference.getDownloadURL().then((url) => console.log(url));
+      });
+    } catch (e) {}
+  };
   componentDidMount() {
     console.log(this.props);
     const db = this.props.firebase.firestore
@@ -53,7 +71,7 @@ class Home extends Component {
         >
           <div className="row">
             <div className="col-md-6 offset-sm-3">
-              <Accordion defaultActiveKey="0">
+              <Accordion defaultActiveKey="1">
                 <Card>
                   <Accordion.Toggle
                     as={Card.Header}
@@ -63,7 +81,11 @@ class Home extends Component {
                     How to play
                   </Accordion.Toggle>
                   <Accordion.Collapse eventKey="0">
-                    <Card.Body>Rules</Card.Body>
+                    <Card.Body>
+                      You are given a partial image and you have 4 tries to
+                      guess the image. To reveal more of the image you can
+                      complete the given tasks.
+                    </Card.Body>
                   </Accordion.Collapse>
                 </Card>
                 <div></div>
@@ -72,8 +94,42 @@ class Home extends Component {
                 <div>
                   <img src={this.state.url} className="img img-fluid" />
                 </div>
-                {JSON.stringify(this.state)}
               </Jumbotron>
+              <form>
+                <div className="form-group text-center">
+                  <label>Make a guess!</label>
+                  <div className="col-sm-5 offset-sm-3 text-center">
+                    <input
+                      className="form-control width-50 mx-10"
+                      type="text"
+                    />
+                  </div>
+                  <small className="form-text text-muted">
+                    You only have {this.state.guesses} guesses remaining !
+                  </small>
+                  <div className="text-center py-3">
+                    <button className="btn btn-primary ">Guess!</button>
+                  </div>
+                </div>
+                <div className="form-group mx-10 text-center">
+                  <label>upload task</label>
+                  <div className="col-sm-5 offset-sm-4 text-center">
+                    <input
+                      className="form-control-file"
+                      type="file"
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                  <div className="text-center py-3">
+                    <button
+                      className="btn btn-primary "
+                      onClick={this.handleUpload}
+                    >
+                      Upload!
+                    </button>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
         </div>
